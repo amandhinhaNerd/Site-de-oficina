cef286e265af21186bd172cf61d6c9e2085fa081
jojo <amandha.rodrigues24@nave.org.br>
2025-08-14 16:04:36 -0300

    wehfjhsfhsdkhf

--- script.js
+++ script.js
@@ -1,44 +1,27 @@
-const btn = document.querySelector('.btn')
-let tema = 'claro'
+const btn = document.querySelector('.btn');
+let tema = 'claro';
 
-const textos = document.querySelectorAll('p')
-const titulos = document.querySelectorAll('h2')
-const titulos2 = document.querySelectorAll('h3')
-const imgTema = document.querySelector('#imgTema')
-
+const textos = document.querySelectorAll('p');
+const titulos = document.querySelectorAll('h2');
+const titulos2 = document.querySelectorAll('h3');
 
 function click() {
     if (tema === 'claro') {
-        tema = "escuro"
-        document.querySelector('body').style.backgroundColor = '#020037'
-        imgTema.style.backgroundColor = 'white'
-        for (let i = 0; i < textos.length; i++) {
-            textos[i].style.color = 'white'
-        }
-        for (let i = 0; i < titulos2.length; i++) {
-            titulos2[i].style.color = 'white'
-        }
-        for (let i = 0; i < titulos.length; i++) {
-            titulos[i].style.color = 'white'
-        }
-        
+        tema = 'escuro';
+        document.body.style.backgroundColor = '#020037';
+        textos.forEach(t => t.style.color = 'white');
+        titulos.forEach(t => t.style.color = 'white');
+        titulos2.forEach(t => t.style.color = 'white');
     } else {
-        tema = 'claro'
-        document.querySelector('body').style.backgroundColor = 'white'
-        imgTema.style.backgroundColor = 'tranparent'
-        for (let i = 0; i < textos.length; i++) {
-            textos[i].style.color = 'white'
-        }
-        for (let i = 0; i < titulos2.length; i++) {
-            titulos2[i].style.color = 'white'
-        }
-        for (let i = 0; i < titulos.length; i++) {
-            titulos[i].style.color = 'white'
-        }
+        tema = 'claro';
+        document.body.style.backgroundColor = 'white';
+        textos.forEach(t => t.style.color = 'black');
+        titulos.forEach(t => t.style.color = 'black');
+        titulos2.forEach(t => t.style.color = 'black');
     }
 }
 
-btn.addEventListener('click', click)
+btn.addEventListener('click', click);
 
 
 const modal = document.querySelector('dialog');
